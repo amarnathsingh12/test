4d79aaaec05b393a4051cfd28fe81f2c3ba1b078
amarnathsingh12 <amarnathroman@gmail.com>
2023-12-28 14:41:13 +0530

    Textform componenet added

--- src/App.js
+++ src/App.js
@@ -2,38 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 import About from './component/About';
 import Navbar from './component/Navbar';
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TextForm from './component/Textform';
 
 function App() {
-  const[mode, setMode] = useState('dark');
+  const [mode, setMode] = useState('dark');
 
-  const toggleMode = ()=>{
-    if(mode === 'light'){
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = 'black';
 
     }
-    else{
+    else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
     }
   }
   return (
     <>
-    <BrowserRouter>
-      <Navbar title= "TextUtils" mode={mode} togglemode={toggleMode}/>
-      <div className="container my-3">
-      <Routes>
-          <Route path="/about" element={<About />}/>
-          {/* <Route path="/">
-          <TextForm heading="Enter your text to analyse"/>
-          </Route> */}
-      </Routes>
-      </div>
-    </BrowserRouter>
-{/* <About/> */}
+      <BrowserRouter>
+        <Navbar title="TextUtils" mode={mode} togglemode={toggleMode} />
+        <div className="container my-3">
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/about" element={
+              <TextForm heading="Enter your text to analyse" />
+            } />
+          </Routes>
+        </div>
+      </BrowserRouter>
+      {/* <About/> */}
     </>
   );
 }
